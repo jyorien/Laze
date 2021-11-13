f6c8c48236509ae296d2b1de17063729d987f83b
Joey Lim <jyorin0206@gmail.com>
2021-11-13 21:16:27 +0800

    created login screen composable

---
+++ app/src/main/java/com/example/laze/screens/LoginScreen.kt
@@ -0,0 +1,10 @@
+package com.example.laze.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginScreen() {
+    Text(text = "hello")
+
+}
