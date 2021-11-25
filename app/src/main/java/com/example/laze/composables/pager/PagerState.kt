4d3ff1aed1d83410de03fee35fb796388c5f6943
joey <jyorin0206@gmail.com>
2021-11-25 17:04:53 +0800

    viewpager works with dummy data

---
+++ app/src/main/java/com/example/laze/composables/pager/PagerState.kt
@@ -0,0 +1,129 @@
+package com.example.laze.composables.pager
+
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.SpringSpec
+import androidx.compose.animation.core.calculateTargetValue
+import androidx.compose.animation.splineBasedDecay
+import androidx.compose.foundation.gestures.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerInputChange
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
+import androidx.compose.ui.input.pointer.util.VelocityTracker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
+import kotlin.math.sign
+
+@Composable
+fun rememberPagerState(): PagerState = remember { PagerState() }
+
+class PagerState {
+    var currentIndex by mutableStateOf(0)
+    var numberOfItems by mutableStateOf(0)
+    var itemFraction by mutableStateOf(0f)
+    var overshootFraction by mutableStateOf(0f)
+    var itemSpacing by mutableStateOf(0f)
+    var itemDimension by mutableStateOf(0)
+    var orientation by mutableStateOf(Orientation.Horizontal)
+    var scope: CoroutineScope? by mutableStateOf(null)
+    var listener: (Int) -> Unit by mutableStateOf({})
+    val dragOffset = Animatable(0f)
+
+    private val animationSpec = SpringSpec<Float>(
+        dampingRatio = Spring.DampingRatioLowBouncy,
+        stiffness = Spring.StiffnessLow,
+    )
+
+    suspend fun snapTo(index: Int) {
+        dragOffset.snapTo(index.toFloat() * (itemDimension + itemSpacing))
+    }
+
+    val inputModifier = Modifier.pointerInput(numberOfItems) {
+        fun itemIndex(offset: Int): Int = (offset / (itemDimension + itemSpacing)).roundToInt()
+            .coerceIn(0, numberOfItems - 1)
+
+        fun updateIndex(offset: Float) {
+            val index = itemIndex(offset.roundToInt())
+            if (index != currentIndex) {
+                currentIndex = index
+                listener(index)
+            }
+        }
+
+        fun calculateOffsetLimit(): OffsetLimit {
+            val dimension = when (orientation) {
+                Orientation.Horizontal -> size.width
+                Orientation.Vertical -> size.height
+            }
+            val itemSideMargin = (dimension - itemDimension) / 2f
+            return OffsetLimit(
+                min = -dimension * overshootFraction + itemSideMargin,
+                max = numberOfItems * (itemDimension + itemSpacing) - (1f - overshootFraction) * dimension + itemSideMargin,
+            )
+        }
+
+        forEachGesture {
+            awaitPointerEventScope {
+                val tracker = VelocityTracker()
+                val decay = splineBasedDecay<Float>(this)
+                val down = awaitFirstDown()
+                val offsetLimit = calculateOffsetLimit()
+                val dragHandler = { change: PointerInputChange ->
+                    scope?.launch {
+                        val dragChange = change.calculateDragChange(orientation)
+                        dragOffset.snapTo(
+                            (dragOffset.value - dragChange).coerceIn(
+                                offsetLimit.min,
+                                offsetLimit.max
+                            )
+                        )
+                        updateIndex(dragOffset.value)
+                    }
+                    tracker.addPosition(change.uptimeMillis, change.position)
+                }
+                when (orientation) {
+                    Orientation.Horizontal -> horizontalDrag(down.id, dragHandler)
+                    Orientation.Vertical -> verticalDrag(down.id, dragHandler)
+                }
+                val velocity = tracker.calculateVelocity(orientation)
+                scope?.launch {
+                    var targetOffset = decay.calculateTargetValue(dragOffset.value, -velocity)
+                    val remainder = targetOffset.toInt().absoluteValue % itemDimension
+                    val extra = if (remainder > itemDimension / 2f) 1 else 0
+                    val lastVisibleIndex =
+                        (targetOffset.absoluteValue / itemDimension.toFloat()).toInt() + extra
+                    targetOffset = (lastVisibleIndex * (itemDimension + itemSpacing) * targetOffset.sign)
+                        .coerceIn(0f, (numberOfItems - 1).toFloat() * (itemDimension + itemSpacing))
+                    dragOffset.animateTo(
+                        animationSpec = animationSpec,
+                        targetValue = targetOffset,
+                        initialVelocity = -velocity
+                    ) {
+                        updateIndex(value)
+                    }
+                }
+            }
+        }
+    }
+
+    data class OffsetLimit(
+        val min: Float,
+        val max: Float,
+    )
+}
+
+private fun VelocityTracker.calculateVelocity(orientation: Orientation) = when (orientation) {
+    Orientation.Horizontal -> calculateVelocity().x
+    Orientation.Vertical -> calculateVelocity().y
+}
+
+private fun PointerInputChange.calculateDragChange(orientation: Orientation) =
+    when (orientation) {
+        Orientation.Horizontal -> positionChange().x
+        Orientation.Vertical -> positionChange().y
+    }
--- app/src/main/java/com/example/laze/screens/ViewPostScreen.kt
+++ app/src/main/java/com/example/laze/screens/ViewPostScreen.kt
@@ -1,9 +1,46 @@
 package com.example.laze.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.laze.composables.Pager
 
 @Composable
 fun ViewPostScreen() {
-    Text("View Posts")
+    val items = listOf(
+        Color.Red,
+        Color.Blue,
+        Color.Green,
+        Color.Yellow,
+        Color.Cyan,
+        Color.Magenta
+    )
+    Pager(
+        items = items,
+        modifier = Modifier.fillMaxSize(), orientation = Orientation.Vertical,
+        itemSpacing = 16.dp,
+        contentFactory = { item ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(item),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = item.toString(),
+                    modifier = Modifier.padding(all = 16.dp),
+                    style = MaterialTheme.typography.h6,
+                )
+            }
+        }
+    )
 }
