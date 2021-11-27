1784623ab05467680b75f0bbc6326125f9064694
Joey Lim <jyorin0206@gmail.com>
2021-11-27 19:27:53 +0800

    data loads into composable. added coil and firebase coroutines

--- app/src/main/java/com/example/laze/data/FirestoreRepo.kt
+++ app/src/main/java/com/example/laze/data/FirestoreRepo.kt
@@ -4,12 +4,14 @@ import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
 
 class FirestoreRepo {
     private val firestore = FirebaseFirestore.getInstance()
     private val auth = FirebaseAuth.getInstance()
+    private val storage = FirebaseStorage.getInstance()
 
     fun getPostDetails() = callbackFlow {
 
--- app/src/main/java/com/example/laze/screens/ViewPostScreen.kt
+++ app/src/main/java/com/example/laze/screens/ViewPostScreen.kt
@@ -1,7 +1,9 @@
 package com.example.laze.screens
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,20 +12,33 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.example.laze.R
 import com.example.laze.composables.Pager
 import com.example.laze.data.MainViewModel
 import com.example.laze.data.OnError
 import com.example.laze.data.OnSuccess
 import com.example.laze.data.Post
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.tasks.await
 
+@OptIn(ExperimentalCoilApi::class)
 @Composable
 fun ViewPostScreen(viewModel: MainViewModel) {
+    val storage = FirebaseStorage.getInstance()
     when (val posts = viewModel.postsStateFlow.asStateFlow().collectAsState().value) {
         is OnError -> {
             Text(text = "Posts couldn't load")
@@ -31,31 +46,31 @@ fun ViewPostScreen(viewModel: MainViewModel) {
 
         is OnSuccess -> {
             val postsList = mutableListOf<Post>()
+            runBlocking {
+                val data = posts.querySnapshot?.documents?.forEach { document ->
+                    val data = document.data
+                    data?.let {
+                        val url = storage.getReference("/${data["imageUrl"]}").downloadUrl.await()
+                        val post = Post("Joye", data["description"].toString(),url.toString())
+                        postsList.add(post)
+                    }
 
-            val data = posts.querySnapshot?.documents?.forEach { document ->
-                val data = document.data
-                data?.let {
-                    val post = Post("Joye", data["description"].toString(), data["imageUrl"].toString())
-                    postsList.add(post)
+
                 }
+                Log.d("hello","success data $data")
             }
-            Log.d("hello","success data $data")
+
 
                 Pager(
         items = postsList,
         modifier = Modifier.fillMaxSize(), orientation = Orientation.Vertical,
-        itemSpacing = 16.dp,
         contentFactory = { item ->
             Box(
                 modifier = Modifier
                     .fillMaxSize(),
                 contentAlignment = Alignment.Center
             ) {
-                Text(
-                    text = item.imageUrl,
-                    modifier = Modifier.padding(all = 16.dp),
-                    style = MaterialTheme.typography.h6,
-                )
+                Image(painter = rememberImagePainter(data = item.imageUrl, builder = { placeholder(R.drawable.sample_placeholder) }), contentDescription = "Image", modifier = Modifier.fillMaxSize(), contentScale = ContentScale.Crop)
             }
         }
     )
