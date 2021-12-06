4fc9e003b102fcdbf16f603cfa337ef7f37f6700
Joey Lim <jyorin0206@gmail.com>
2021-12-06 16:08:44 +0800

    done summary page ui

---
+++ app/src/main/java/com/example/laze/screens/UserPostDetailScreen.kt
@@ -0,0 +1,59 @@
+package com.example.laze.screens
+
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.example.laze.R
+import com.example.laze.data.Post
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.tasks.await
+
+@ExperimentalCoilApi
+@Composable
+fun UserPostDetailScreen(data: String, navController: NavController) {
+    val storage = FirebaseStorage.getInstance()
+    val dataList = data.split("$")
+    var post: Post
+    val imgUrl = dataList[2] + "?" + dataList[3]
+    runBlocking {
+        Log.d("hello","img $imgUrl")
+        val url = storage.getReference("/${imgUrl}").downloadUrl.await()
+        post = Post(dataList[0], dataList[1], url.toString())
+    }
+
+    Scaffold() {
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(painter = rememberImagePainter(data = post.imageUrl, builder = { placeholder(R.drawable.sample_placeholder) }), contentDescription = "Image", modifier = Modifier.fillMaxSize(), contentScale = ContentScale.Crop)
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomStart)
+                .padding(20.dp)
+                .height(190.dp) ) {
+                Column {
+                    Text(text = post.username, color = Color.White, fontSize = 36.sp, fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Text(text = post.description, color = Color.White)
+                }
+
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/laze/tabs/PostedTab.kt
+++ app/src/main/java/com/example/laze/tabs/PostedTab.kt
@@ -12,40 +12,62 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.laze.composables.ErrandItemLayout
 import com.example.laze.data.MainViewModel
 import com.example.laze.data.OnError
 import com.example.laze.data.OnSuccess
 import com.example.laze.data.Post
+import com.example.laze.screens.UserPostDetailScreen
 import kotlinx.coroutines.flow.asStateFlow
 
 @Composable
 fun PostedTab() {
     val viewModel = viewModel<MainViewModel>()
     Log.d("hello","Posted Tab")
-    when (val posts = viewModel.userPostsStateFlow.asStateFlow().collectAsState().value) {
-        is OnSuccess -> {
-            val postsList = mutableListOf<Post>()
-            posts.querySnapshot?.documents?.forEach {
-                val post = Post(username = it["name"].toString(), description = it["description"].toString(), imageUrl = it["imageUrl"].toString())
-                postsList.add(post)
+
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "Start") {
+        composable("Detail/{data}", arguments = listOf(navArgument("data") {type = NavType.StringType})) { backStackEntry ->
+            backStackEntry.arguments?.getString("data")?.let { data ->
+                UserPostDetailScreen(data, navController)
+
             }
-            Column(modifier = Modifier.fillMaxSize()) {
-                LazyColumn() {
-                    items(postsList) { currentPost ->
-                        ErrandItemLayout(post = currentPost, onClick = {
-                            Log.d("hello","current post: $currentPost")
-                        })
+        }
+        composable("Start") {
+            when (val posts = viewModel.userPostsStateFlow.asStateFlow().collectAsState().value) {
+                is OnSuccess -> {
+                    val postsList = mutableListOf<Post>()
+                    posts.querySnapshot?.documents?.forEach {
+                        val post = Post(username = it["name"].toString(), description = it["description"].toString(), imageUrl = it["imageUrl"].toString())
+                        postsList.add(post)
+                    }
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        LazyColumn() {
+                            items(postsList) { currentPost ->
+                                ErrandItemLayout(post = currentPost, onClick = {
+                                    Log.d("hello","current post: $currentPost")
+                                    val splitUrl = currentPost.imageUrl.split("?")
+                                    val postDetails = "${currentPost.username}$${currentPost.description}$${splitUrl[0]}$${splitUrl[1]}"
+                                    navController.navigate("Detail/$postDetails")
+                                })
+                            }
+                        }
+
                     }
                 }
 
+                is OnError -> {
+
+                }
             }
         }
-
-        is OnError -> {
-
-        }
     }
+
 
 
 }
