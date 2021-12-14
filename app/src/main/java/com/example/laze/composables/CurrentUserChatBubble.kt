5fbbfc1397887a28c51c5032a4814ac4e34dbbd4
Joey Lim <jyorin0206@gmail.com>
2021-12-14 18:08:16 +0800

    designed chat bubble composables

---
+++ app/src/main/java/com/example/laze/composables/CurrentUserChatBubble.kt
@@ -0,0 +1,39 @@
+package com.example.laze.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CurrentUserChatBubble(currentUserText: String) {
+    Box(Modifier.padding(16.dp)) {
+        Column(
+            modifier = Modifier.background(
+                color = Color(5,96,98),
+                shape = RoundedCornerShape(
+                    topStart = 5.dp,
+                    topEnd = 5.dp,
+                    bottomEnd = 0.dp,
+                    bottomStart = 5.dp
+                )
+            ).padding(horizontal = 10.dp, vertical = 5.dp).widthIn(0.dp, 250.dp)
+        ) {
+            Text(currentUserText, color = Color.White)
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun Preview_current_user_bubble() {
+    CurrentUserChatBubble("hello")
+}
